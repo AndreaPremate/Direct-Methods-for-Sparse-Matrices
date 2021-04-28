811bb1d7b24181729dcb5be2f158ea6fee6148c3
DavideMammarella <mammarelladavide@gmail.com>
2021-04-28 11:38:13 +0200

    Add main.R

---
+++ R/main.R
@@ -0,0 +1,9 @@
+# Title     : Direct Methods for Sparse Matrices
+# Objective : Solve sparse matrices using an open source programming language (R),
+#             a project for the Scientific Computing Methods course of the University of Milan Bicocca
+# Created by: Mammarella Davide
+
+# Packages ----------------------------------------------------------------------
+install.packages("pacman")
+library(pacman)
+pacman::p_load(here, R.matlab, Matrix)
