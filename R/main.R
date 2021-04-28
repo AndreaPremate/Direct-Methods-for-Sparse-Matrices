284206113a3ba6abb1ab57e96ee2ff1309aed045
DavideMammarella <mammarelladavide@gmail.com>
2021-04-28 12:52:16 +0200

    R: read matrices and check their memory requirements (Positive Matrices)

--- R/main.R
+++ R/main.R
@@ -6,4 +6,17 @@
 # Packages ----------------------------------------------------------------------
 install.packages("pacman")
 library(pacman)
-pacman::p_load(here, R.matlab, Matrix)
+pacman::p_load(here, Matrix)
+
+# Reading Matrices (mtx) ---------------------------------------------------------
+bundleAdj <- readMM('matrices/positive/bundle_adj.mtx')
+g3Circuit <- readMM('matrices/positive/G3_circuit.mtx')
+hook1498 <- readMM('matrices/positive/Hook_1498.mtx')
+nd24k <- readMM('matrices/positive/nd24k.mtx')
+# The matrices are already in sparse format so they do not need to be converted.
+
+# Memory (RAM) required from every matrix
+object.size(bundleAdj)
+object.size(g3Circuit)
+object.size(hook1498)
+object.size(nd24k)
