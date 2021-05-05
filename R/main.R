004234f4fa209875cf51e6b3a36cf19c82230e82
DavideMammarella <mammarelladavide@gmail.com>
2021-05-05 16:18:46 +0200

    R: fix packages install and load

--- R/main.R
+++ R/main.R
@@ -4,27 +4,34 @@
 # Created by: Mammarella Davide
 
 # Packages ----------------------------------------------------------------------
-install.packages("pacman")
-library(pacman)
-pacman::p_load(here, parallel, MASS, Matrix)
+#package names
+packages <- c("here","stringr","pryr","Matrix")
+# install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+# load packages
+invisible(lapply(packages, library, character.only = TRUE))
+cat("\n==================================================\n")
 
 # Reading Matrices (mtx) ---------------------------------------------------------
 #hook_1498 <- readMM('matrices/positive/Hook_1498.mtx')
-g3_circuit <- readMM('matrices/positive/G3_circuit.mtx')
+#g3_circuit <- readMM('matrices/positive/G3_circuit.mtx')
 #nd24k <- readMM('matrices/positive/nd24k.mtx')
 #bundle_adj <- readMM('matrices/positive/bundle_adj.mtx')
 #ifiss_mat <- readMM('matrices/non_positive/ifiss_mat.mtx')
 #tsc_opf_1047 <- readMM('matrices/non_positive/tsc_opf_1047.mtx')
 #ns3Da <- readMM('matrices/non_positive/ns3Da.mtx')
-#gt01r <- as.matrix.csr(readMM('matrices/non_positive/gt01r.mtx'))
+gt01r <- readMM('matrices/non_positive/gt01r.mtx')
 # matrices are already in sparse format so they don't need to be converted
 
 # Memory (RAM) occupied by each matrix
 #cat("\n==================================================\n")
 #cat("Hook_1498 memory usage: ")
 #print(object.size(hook_1498), units="Mb")
-cat("G3_circuit memory usage: ")
-print(object.size(g3_circuit), units="Mb")
+#cat("G3_circuit memory usage: ")
+#print(object.size(g3_circuit), units="Mb")
 #cat("nd24k memory usage: ")
 #print(object.size(nd24k), units="Mb")
 #cat("bundle_adj memory usage: ")
@@ -35,26 +42,28 @@ print(object.size(g3_circuit), units="Mb")
 #print(object.size(tsc_opf_1047), units="Mb")
 #cat("ns3Da memory usage: ")
 #print(object.size(ns3Da), units="Mb")
-#cat("GT01R memory usage: ")
-#print(object.size(gt01r), units="Mb")
+cat("GT01R memory usage: ")
+print(object_size(gt01r))
 
 # Solving Matrices ---------------------------------------------------------------
 # Compute B (such that the exact solution of Ax=b is xe=[1,1,..])
-A <- g3_circuit
+
+A <- gt01r
 xe <- rep(1,nrow(A))
 b <- A*xe
 
 # Solve Ax=b
-#start_time <- Sys.time()
-x <- solve(A,b)
-#end_time <- Sys.time()
+start_time <- Sys.time()
+x <- solve(A, b)
+end_time <- Sys.time()
 
 # Metrics ------------------------------------------------------------------------
-#cat("\n==================================================\n")
+cat("\n==================================================\n")
 #time
-#cat("Execution time: ")
+cat("Execution time: ")
 #execution_time <- end_time-start_time
+#execution_time
 #relative error
+
 #solution
 #memory usage
-#cat("Memory usage: ")
