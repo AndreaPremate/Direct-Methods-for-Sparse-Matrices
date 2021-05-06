97c472061e755df1fa4fd6de3676f89b0b8435a1
DavideMammarella <mammarelladavide@gmail.com>
2021-05-06 14:31:49 +0200

    R: add results output in csv file

--- R/main.R
+++ R/main.R
@@ -18,23 +18,15 @@ invisible(lapply(packages, library, character.only = TRUE))
 # Reading Matrices (mtx) ---------------------------------------------------------
 cat("\n==================================================\n")
 
-load_matrices <- function(){
-  cl <- parallel::makeCluster(num_cores, setup_strategy = "sequential")
-  doParallel:: registerDoParallel(cl)
-
-  hook_1498 <- readMM('matrices/positive/Hook_1498.mtx')
-  g3_circuit <- readMM('matrices/positive/G3_circuit.mtx')
-  nd24k <- readMM('matrices/positive/nd24k.mtx')
-  bundle_adj <- readMM('matrices/positive/bundle_adj.mtx')
-  ifiss_mat <- readMM('matrices/non_positive/ifiss_mat.mtx')
-  tsc_opf_1047 <- readMM('matrices/non_positive/tsc_opf_1047.mtx')
-  ns3Da <- readMM('matrices/non_positive/ns3Da.mtx')
-  gt01r <- readMM('matrices/non_positive/gt01r.mtx')
-  # matrices are already in sparse format so they don't need to be converted
-
-  parallel::stopCluster(cl)
-}
-load_matrices()
+hook_1498 <- readMM('matrices/positive/Hook_1498.mtx')
+g3_circuit <- readMM('matrices/positive/G3_circuit.mtx')
+nd24k <- readMM('matrices/positive/nd24k.mtx')
+bundle_adj <- readMM('matrices/positive/bundle_adj.mtx')
+ifiss_mat <- readMM('matrices/non_positive/ifiss_mat.mtx')
+tsc_opf_1047 <- readMM('matrices/non_positive/tsc_opf_1047.mtx')
+ns3Da <- readMM('matrices/non_positive/ns3Da.mtx')
+gt01r <- readMM('matrices/non_positive/gt01r.mtx')
+# matrices are already in sparse format so they don't need to be converted
 
 # data preparation for loops
 matrices <- c("hook_1498", "g3_circuit", "nd24k", "bundle_adj", "ifiss_mat", "tsc_opf_1047", "ns3Da", "gt01r")
@@ -47,13 +39,6 @@ num_cores <- detectCores()-1
 #  matrix<-get(matrices[i])
 #  print(object_size(matrix))
 #}
-
-#read_matrices <- function(input_matrices, i){
-#  #cat(input_matrices[i], "memory size: ")
-#  matrix<-get(input_matrices[i])
-#  print(object_size(matrix))
-#}
-#mclapply(n_matrices, read_matrices(matrices, i), mc.cores = num_cores)
 
 read_matrices <- function(input_matrices, num_matrices){
   cl <- parallel::makeCluster(num_cores, setup_strategy = "sequential")
@@ -68,7 +53,7 @@ read_matrices <- function(input_matrices, num_matrices){
   parallel::stopCluster(cl)
 }
 read_matrices(matrices, n_matrices)
-
+# could mclapply be a better choice?
 
 # Solving Matrices ---------------------------------------------------------------
 
@@ -82,22 +67,23 @@ start_time <- proc.time()
 ram_used <- peakRAM(x <- solve(A, b))
 execution_time <- proc.time()-start_time
 
-# Metrics ------------------------------------------------------------------------
-cat("\n==================================================\n")
+# Metrics CLI output -------------------------------------------------------------
+cat("\n==================================================\n \n")
 
-# time (rbenchmark/microbenchmart not used because executions can last over 5 minute)
-cat("Execution Time (sec): ")
-cat(execution_time[["elapsed"]], "\n")
-# elapsed time = CPU execution time + external processes (OS)
+# time (sec) (rbenchmark/microbenchmart not used because executions can last over 5 minute)
+time <- execution_time[["elapsed"]]
 
-# relative error
-#cat("Relative Error (norm2): ")
-#relative_error <- norm(x-xe, "2")/norm(xe, "2")
-#cat(relative_error, "\n")
+# relative error (norm2)
+relative_error <- norm(x-xe, "2")/norm(xe, "2")
 
-# RAM used
-cat("RAM used (MiB): ")
-cat(ram_used$Peak_RAM_Used_MiB, "\n")
-# I do not extract the elapsed time of this function because it does not consider the time that elapses between the external processes of the OS (it considers only CPU execution time)
+# RAM used (MB)
+ram <- (ram_used$Total_RAM_Used_MiB)*(1.048576)
 
+# Metrics CSV Output -------------------------------------------------------------
+results <- data.frame(df_matrix=character(), df_time=double(), df_relative_error=double(), df_ram=double())
+result_gt01r <- list(df_matrix="gt01r", df_time=time, df_relative_error=relative_error, df_ram=ram)
+results <- rbind(results, result_gt01r, stringsAsFactors = FALSE)
+results
 
+#path <- paste0(here("results"), "/")
+write.csv(results, here("r_results"), row.names = FALSE)
