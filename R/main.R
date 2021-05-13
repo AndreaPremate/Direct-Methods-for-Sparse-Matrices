10ba0a9ec5bcefddcaabf5cde3be64d67f5d141a
DavideMammarella <mammarelladavide@gmail.com>
2021-05-13 11:44:06 +0200

    R: add ALL results in SI, fix CSV output

--- R/main.R
+++ R/main.R
@@ -4,20 +4,22 @@
 # Created by: Mammarella Davide
 
 # Packages ----------------------------------------------------------------------
-chooseCRANmirror(ind=1)
-#package names / pryr rimosso
-packages <- c("here","peakRAM","Matrix","SparseM")
 options(warn=-1)
-# install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
-# load packages
-invisible(lapply(packages, library, character.only = TRUE))
+suppressMessages({
+  chooseCRANmirror(ind=1)
+  #package names
+  packages <- c("here","bench","Matrix","SparseM")
+  # install packages not yet installed
+  installed_packages <- packages %in% rownames(installed.packages())
+  if (any(installed_packages == FALSE)) {
+    install.packages(packages[!installed_packages])
+  }
+  # load packages
+  invisible(lapply(packages, library, character.only = TRUE, quietly=T))
+})
 
 # Tables initialization for CSV --------------------------------------------------
-matrix_reading_results <- data.frame(df_matrix=character(), df_loading_time=double(), df_memory_size=double())
+matrix_reading_results <- data.frame(df_matrix=character(), df_loading_time=double(), df_matrix_size=double(), df_memory_allocation=double())
 matrix_solving_results <- data.frame(df_matrix=character(), df_time=double(), df_relative_error=double(), df_peak_ram=double())
 
 # Reading Matrices (mtx) ---------------------------------------------------------
@@ -28,33 +30,34 @@ list_matrices <- sub(".mtx$", "", list_matrices_mtx) #matrices names for loop pu
 num_matrices <- length(list_matrices) #number of matrices for loop purpose
 
 # Matrix reading loop (automatic name generation and matrix assignation)
-cat("\n\nReading matrices...\n")
+cat("\nReading matrices...")
 for (i in 1:num_matrices){
-    cat("\n==================================================\n")
-    cat("Reading", list_matrices[i], "matrix:")
-    matrix_name <- sub(".mtx$", "", list_matrices_mtx[i])
-    matrix_dir <- paste0(matrices_dir, list_matrices_mtx[i], sep="")
-    start_loading_time <- proc.time()
-    matrix_read <- readMM(matrix_dir)
-    matrix_loading_time <- proc.time()-start_loading_time
-    assign(paste0(matrix_name), matrix_read)
+  cat("\n==================================================\n")
+  cat("Reading", list_matrices[i], "matrix:")
+  matrix_name <- sub(".mtx$", "", list_matrices_mtx[i])
+  matrix_dir <- paste0(matrices_dir, list_matrices_mtx[i], sep="")
+  matrix_read_benchmark <- mark(matrix_read <- readMM(matrix_dir))
+  assign(paste0(matrix_name), matrix_read)
 
-    cat("\n Memory size (MB): ")
-    matrix_size <- format(object.size(matrix_read), units="MB")
-    cat(matrix_size)
-    cat("\n Loading time (s): ")
-    matrix_load_time <- matrix_loading_time[["elapsed"]]
-    cat(matrix_load_time)
+  cat("\n Matrix size (Bytes): ")
+  matrix_size <- as.numeric(object.size(matrix_read))
+  cat(matrix_size)
+  cat("\n Reading memory allocation (Bytes): ")
+  matrix_allocation <- as.numeric(matrix_read_benchmark$mem_alloc)
+  cat(matrix_allocation)
+  cat("\n Loading time (s): ")
+  matrix_load_time <- matrix_read_benchmark$total_time #time in s
+  cat(matrix_load_time)
 
-    # Matrix reading results in dataframe
-    matrix_reading_data <- list(df_matrix=list_matrices[i], df_loading_time=matrix_load_time, df_memory_size=matrix_size)
-    matrix_reading_results <- rbind(matrix_reading_results, matrix_reading_data, stringsAsFactors = FALSE)
+  # Matrix reading results in dataframe
+  matrix_reading_data <- list(df_matrix=list_matrices[i], df_loading_time=matrix_load_time, df_matrix_size=matrix_size, df_memory_allocation=matrix_allocation)
+  matrix_reading_results <- rbind(matrix_reading_results, matrix_reading_data, stringsAsFactors = FALSE)
 }
 cat("\n==================================================")
-cat("\n\nMatrices reading finished!")
+cat("\nMatrices reading finished!")
 
 # Solving Matrices ---------------------------------------------------------------
-cat("\n\nSolving matrices...\n")
+cat("\n\nSolving matrices...")
 for(i in 1:num_matrices){
   cat("\n==================================================\n")
   cat("Solving", list_matrices[i], "matrix:")
@@ -66,11 +69,11 @@ for(i in 1:num_matrices){
 
   # solve Ax=b
   start_time <- proc.time()
-  ram_used <- peakRAM(x <- SparseM::solve(A, b))
+  x_benchmark <- mark(x <- SparseM::solve(A, b))
   execution_time <- proc.time()-start_time
 
   # time (sec)
-  time <- as.numeric(execution_time[["elapsed"]]) #time in s
+  time <- x_benchmark$total_time #time in s
   conv_time <- time/60 #time in min
   cat("\n Execution Time (min): ", conv_time)
 
@@ -78,22 +81,20 @@ for(i in 1:num_matrices){
   relative_error <- norm(x-xe,"2")/norm(xe,"2")
   cat("\n Relative Error (norm2): ", relative_error)
 
-  # Peak RAM used (MiB)
-  peak_ram <- ram_used$Peak_RAM_Used_MiB #ram in MiB
-  conv_ram <- format(peak_ram, units="MB") #ram in MB
-  cat("\n RAM Used: ")
-  cat("\n  Peak Ram (MB)", conv_ram)
+  # Peak RAM used (MB)
+  conv_ram <- as.numeric(x_benchmark$mem_alloc)
+  cat("\n RAM used (Bytes): ", conv_ram)
 
   # Matrix solving results in dataframe
   matrix_solving_data <- list(df_matrix=list_matrices[i], df_time=conv_time, df_relative_error=relative_error, df_peak_ram=conv_ram)
   matrix_solving_results <- rbind(matrix_solving_results, matrix_solving_data, stringsAsFactors = FALSE)
 
   # free memory (dealing with big matrix could generate memory errors)
-  rm(list=c("A","xe","b","x","time","relative_error","total_ram","peak_ram"))
+  rm(list=c("A","xe","b","x_benchmark","time", "conv_time", "relative_error","conv_ram"))
   gc() #called after rm as suggested in R documentation
 }
 cat("\n==================================================")
-cat("\n\nMatrices solving finished!")
+cat("\nMatrices solving finished!")
 
 # Metrics CSV output -------------------------------------------------------------
 write.csv(matrix_reading_results, file.path(results_dir, "matrix_reading_results.csv"), row.names = FALSE)
