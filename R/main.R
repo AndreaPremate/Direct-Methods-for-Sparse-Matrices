aa79bf000293c169be5fd4dca628e5a1f4b4288b
DavideMammarella <mammarelladavide@gmail.com>
2021-05-26 18:34:02 +0200

    R: minor fix

--- R/main.R
+++ R/main.R
@@ -9,7 +9,7 @@ cat("\nInitializing the environment...")
 # Auto-Mirror + Install only packages not yet installed
 suppressMessages({
   chooseCRANmirror(ind=1)
-  packages <- c("here","bench","unix","tryCatchLog","Matrix")
+  packages <- c("here","bench","tryCatchLog","Matrix")
   installed_packages <- packages %in% rownames(installed.packages())
   if (any(installed_packages == FALSE)) {
     install.packages(packages[!installed_packages])
