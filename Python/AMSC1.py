269818b9047ab5daabd6915f204d4a684292887e
Davide Pietrasanta <davide.pietrasanta@virgilio.it>
2021-05-28 11:12:34 +0200

    Update Cholesky

--- Python/AMSC1.py
+++ Python/AMSC1.py
@@ -1,38 +1,47 @@
 #AMSC1
-import scipy as sp
-import numpy as np
 import pathlib
 import time
+import psutil #Just for windows
+import numpy as np
 
-from scipy.io import mmread
+#scipy
+import scipy as sp
+from scipy.io import mmread 
 from scipy.sparse import isspmatrix, isspmatrix_csc, isspmatrix_csr
 from scipy.sparse.linalg import spsolve
 from scipy.linalg import norm
 from scipy.sparse import csc_matrix
 
-import psutil #Just for windows
+#sksparse
+from sksparse.cholmod import cholesky
+
+
+
 
 
 path = str(pathlib.Path(__file__).parent.absolute())
 
 def Matrice_input(path, name_matrix, name_save_file):
+    print(name_save_file[1:])
     p = psutil.Process()
     start = time.time()
     A = mmread(path + name_matrix)
     end = time.time()
     time_load = end - start
-    #print(isspmatrix(A)) #Check is matrix is sparse
-    #print(isspmatrix_csc(A))
-    #print(isspmatrix_csr(A))
     n_col = A.get_shape()[0]
     x = np.ones(n_col)
     b = A * x
     start = time.time()
 
-    if(isspmatrix_csc(A) or isspmatrix_csr(A) ):
-        x_approx = spsolve(A, b)
-    else:
-        x_approx = spsolve(csc_matrix(A), b)
+    
+    try:    # Vedere se e' candidato per Cholesky
+        factor = cholesky(A)
+        x_approx = factor(b)
+    except:
+        if(isspmatrix_csc(A) or isspmatrix_csr(A) ):
+            x_approx = spsolve(A, b)
+        else:
+            x_approx = spsolve(csc_matrix(A), b)
 
     end = time.time()
     time_solution = end - start
@@ -59,13 +68,15 @@ def Matrice_input(path, name_matrix, name_save_file):
 
     return solution
 
-Matrice_input(path, "\\GT01R\\GT01R.mtx", "\\GT01R.txt" ) 
-Matrice_input(path, "\\TSC_OPF_1047\\TSC_OPF_1047.mtx", "\\TSC_OPF_1047.txt" )
-Matrice_input(path, "\\ns3Da\\ns3Da.mtx", "\\ns3Da.txt" ) 
-Matrice_input(path, "\\bundle_adj\\bundle_adj.mtx", "\\bundle_adj.txt" ) 
-Matrice_input(path, "\\ifiss_mat\\ifiss_mat.mtx", "\\ifiss_mat.txt" )
-Matrice_input(path, "\\G3_circuit\\G3_circuit.mtx", "\\G3_circuit.txt" ) 
-Matrice_input(path, "\\nd24k\\nd24k.mtx", "\\nd24k.txt" ) # Va ma lento
+#Matrice_input(path, "\\GT01R\\GT01R.mtx", "\\GT01R.txt" ) 
+#Matrice_input(path, "\\TSC_OPF_1047\\TSC_OPF_1047.mtx", "\\TSC_OPF_1047.txt" )
+#Matrice_input(path, "\\ns3Da\\ns3Da.mtx", "\\ns3Da.txt" ) 
+#Matrice_input(path, "\\ifiss_mat\\ifiss_mat.mtx", "\\ifiss_mat.txt")
 
-Matrice_input(path, "\\Hook_1498\\Hook_1498.mtx", "\\Hook_1498.txt" ) # Can't expand MemType 0: jcol 649435
+#Matrice_input(path, "\\bundle_adj\\bundle_adj.mtx", "\\bundle_adj.txt" ) #Positive 
+#Matrice_input(path, "\\G3_circuit\\G3_circuit.mtx", "\\G3_circuit.txt" ) #Positive
+#Matrice_input(path, "\\nd24k\\nd24k.mtx", "\\nd24k.txt" ) #Positive # Va ma lento 
+
+#Matrice_input(path, "\\Hook_1498\\Hook_1498.mtx", "\\Hook_1498.txt" ) #Positive 
+# Can't expand MemType 0: jcol 649435 
 
