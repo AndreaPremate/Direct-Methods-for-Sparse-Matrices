ef0cfdd4a4878602a0b8149769b4a6f06f67e30f
AndreaPremate <79997203+AndreaPremate@users.noreply.github.com>
2021-05-29 10:09:44 +0200

    Add files via upload

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <Eigen/Sparse>
 #include <unsupported/Eigen/SparseExtra>
+//#include <Eigen/SparseLU>
 #include <chrono>
+//#include <Eigen/SparseCholesky>
 #include <Eigen/Core>
 #include <fstream>
 #include <string>
@@ -79,18 +81,22 @@ void solve_CHOLESKY(const char *__s){
     std::chrono::steady_clock::time_point end_load = std::chrono::steady_clock::now();               //end load
     filewrite << "Time spent loading the matrix = " << std::chrono::duration_cast<std::chrono::seconds>(end_load - begin_load).count() << "[s]" << std::endl;
     Resulting_Matrix.makeCompressed();
+   
     Eigen::VectorXd xe = Eigen::VectorXd::Ones(Resulting_Matrix.cols());
     Eigen::VectorXd b = Resulting_Matrix*xe;
+
+    //CHOLESKY DECOMPOSITION
     std::chrono::steady_clock::time_point begin_LDLT = std::chrono::steady_clock::now();             //start LDLT
 
     Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>   solverLDLT;
     solverLDLT.analyzePattern(Resulting_Matrix);
     solverLDLT.factorize(Resulting_Matrix);
     Eigen::VectorXd xLDLT = solverLDLT.solve(b);
-    /*
-    if(solverLDLT.info() == Eigen::Success)
+    
+    /*if(solverLDLT.info() == Eigen::NumericalIssue)
         std::cout << "LDLT SUCCESS" << std::endl;
     */
+
     std::chrono::steady_clock::time_point end_LDLT = std::chrono::steady_clock::now();               //end LU LDLT
 
     filewrite << "Time spent using LDLT decomposition = " << std::chrono::duration_cast<std::chrono::seconds>(end_LDLT - begin_LDLT).count() << "[s]" << std::endl;
@@ -113,12 +119,12 @@ void solve_CHOLESKY(const char *__s){
     std::ofstream writeSolution;
     std::string sol = " solution.txt";
     writeSolution.open(__s + sol);
-    writeSolution << xLDLT << std::endl;
+    writeSolution << xLDLT - xe << std::endl;
     writeSolution.close();
 }
 
 
-void solve_LU(const char *__s){
+void solve_LU(const char *__s, std::string symmetric){
 
     //MATRIX AND XE LOADING
 
@@ -130,7 +136,16 @@ void solve_LU(const char *__s){
     SMatrixXd A;
     std::chrono::steady_clock::time_point begin_load = std::chrono::steady_clock::now();             //start load
     Eigen::loadMarket(A, __s);
-    Eigen::SparseMatrix<double> Resulting_Matrix = A;      //PER MATRICI NON SIMMETRICHE
+    Eigen::SparseMatrix<double> Resulting_Matrix;
+    if(symmetric.compare("non_symmetric") == 0)
+        Resulting_Matrix = A;      //PER MATRICI NON SIMMETRICHE
+    else if(symmetric.compare("symmetric") == 0)
+        Resulting_Matrix = A.selfadjointView<Eigen::Lower>();
+    else {
+        std::cout << "Second paramater error"<< std::endl;
+        return ; 
+    }
+
     std::chrono::steady_clock::time_point end_load = std::chrono::steady_clock::now();               //end load
     filewrite << "Time spent loading the matrix = " << std::chrono::duration_cast<std::chrono::seconds>(end_load - begin_load).count() << "[s]" << std::endl;
     Resulting_Matrix.makeCompressed();
@@ -142,9 +157,11 @@ void solve_LU(const char *__s){
 
     std::chrono::steady_clock::time_point begin_LU = std::chrono::steady_clock::now();             //start LU
 
-    Eigen::SparseLU<Eigen::SparseMatrix<double>>   solverLU; //, Eigen::COLAMDOrdering<int> 
+    Eigen::SparseLU<Eigen::SparseMatrix<double>> solverLU; //, Eigen::COLAMDOrdering<int> 
     solverLU.analyzePattern(Resulting_Matrix);
     solverLU.factorize(Resulting_Matrix);
+    if (solverLU.info() == Eigen::NumericalIssue)
+        std::cout<< solverLU.info() << Eigen::NumericalIssue <<  std::endl;
     Eigen::VectorXd xLU = solverLU.solve(b);
 
     std::chrono::steady_clock::time_point end_LU = std::chrono::steady_clock::now();               //end LU
@@ -169,8 +186,9 @@ void solve_LU(const char *__s){
     std::ofstream writeSolution;
     std::string sol = " solution.txt";
     writeSolution.open(__s + sol);
-    writeSolution << xLU << std::endl;
+    writeSolution << xLU - xe << std::endl;
     writeSolution.close();
+
 }
 
 
@@ -178,12 +196,12 @@ int main(){
     
     //eseguire una matrice alla volta per stabilire il picco di ram esatto(sennò torna solo quello maggiore)
 
-    solve_LU("GT01R.mtx");
-    //solve_LU("TSC_OPF_1047.mtx");
-    //solve_LU("ns3Da.mtx");
+    //solve_LU("GT01R.mtx", "non_symmetric");
+    //solve_LU("TSC_OPF_1047.mtx", "symmetric");
+    //solve_LU("ns3Da.mtx", "non_symmetric");
     //solve_CHOLESKY("bundle_adj.mtx");
-    //solve_LU("ifiss_mat.mtx");
-    //solve_CHOLESKY("G3_circuit.mtx");
+    //solve_LU("ifiss_mat.mtx", "non_symmetric");
+    solve_CHOLESKY("G3_circuit.mtx");
     //solve_CHOLESKY("nd24k.mtx");
     //solve_CHOLESKY("Hook_1498.mtx");
 }
