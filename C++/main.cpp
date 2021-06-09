df86d051da2c03c6c401cba4afdf15e1b9eb587a
AndreaPremate <79997203+AndreaPremate@users.noreply.github.com>
2021-06-09 17:32:38 +0200

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -65,71 +65,12 @@ int PrintMemoryInfo( DWORD processID ){
 //fine linux*/
 
 
-void solve_CHOLESKY(const char *__s){
-    //MATRIX AND XE LOADING
-
-    std::ofstream filewrite;
-    std::string res = " Results_Cholesky.txt";
-    filewrite.open(__s + res);
-
-    typedef Eigen::SparseMatrix<double> SMatrixXd;
-    SMatrixXd A;
-    std::chrono::steady_clock::time_point begin_load = std::chrono::steady_clock::now();             //start load
-    Eigen::loadMarket(A, __s);
-    Eigen::SparseMatrix<double> Resulting_Matrix = A.selfadjointView<Eigen::Lower>();
-    //Eigen::SparseMatrix<double> result2 = A;
-    std::chrono::steady_clock::time_point end_load = std::chrono::steady_clock::now();               //end load
-    filewrite << "Time spent loading the matrix = " << std::chrono::duration_cast<std::chrono::seconds>(end_load - begin_load).count() << "[s]" << std::endl;
-    Resulting_Matrix.makeCompressed();
-   
-    Eigen::VectorXd xe = Eigen::VectorXd::Ones(Resulting_Matrix.cols());
-    Eigen::VectorXd b = Resulting_Matrix*xe;
-
-    //CHOLESKY DECOMPOSITION
-    std::chrono::steady_clock::time_point begin_LDLT = std::chrono::steady_clock::now();             //start LDLT
-
-    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>   solverLDLT;
-    solverLDLT.analyzePattern(Resulting_Matrix);
-    solverLDLT.factorize(Resulting_Matrix);
-    Eigen::VectorXd xLDLT = solverLDLT.solve(b);
-    
-    /*if(solverLDLT.info() == Eigen::NumericalIssue)
-        std::cout << "LDLT SUCCESS" << std::endl;
-    */
-
-    std::chrono::steady_clock::time_point end_LDLT = std::chrono::steady_clock::now();               //end LU LDLT
-
-    filewrite << "Time spent using LDLT decomposition = " << std::chrono::duration_cast<std::chrono::seconds>(end_LDLT - begin_LDLT).count() << "[s]" << std::endl;
-
-    double relative_errorLDLT = (xLDLT - xe).norm() / xe.norm();
-    filewrite << "The relative error using LDLT decomposition is: " << relative_errorLDLT << std::endl;
-
-    //windows
-    filewrite << "PeakWorkingSetSize(windows): " << PrintMemoryInfo(GetCurrentProcessId()) << " MB"<< std::endl;
-	 //fine windows 
-	
-    /*//linux
-    struct rusage rusage;
-    getrusage( RUSAGE_SELF, &rusage );
-    filewrite << "PeakWorkingSetSize(linux): " << (size_t)(rusage.ru_maxrss * 1024L)/1000000 << " MB"<< std::endl;
-	//fine linux*/
-	
-    filewrite.close();
-
-    std::ofstream writeSolution;
-    std::string sol = " solution.txt";
-    writeSolution.open(__s + sol);
-    writeSolution << xLDLT - xe << std::endl;
-    writeSolution.close();
-}
-
-
-void solve_LU(const char *__s, std::string symmetric){
+void solve(const char *__s, std::string symmetric){
 
     //MATRIX AND XE LOADING
 
     std::ofstream filewrite;
-    std::string res = " Results_LU.txt";
+    std::string res = " Results.txt";
     filewrite.open(__s + res);
 
     typedef Eigen::SparseMatrix<double> SMatrixXd;
@@ -153,55 +94,102 @@ void solve_LU(const char *__s, std::string symmetric){
     Eigen::VectorXd xe = Eigen::VectorXd::Ones(Resulting_Matrix.cols());
     Eigen::VectorXd b = Resulting_Matrix*xe;
 
-    //LU DECOMPOSITION
 
-    std::chrono::steady_clock::time_point begin_LU = std::chrono::steady_clock::now();             //start LU
 
-    Eigen::SparseLU<Eigen::SparseMatrix<double>> solverLU; //, Eigen::COLAMDOrdering<int> 
-    solverLU.analyzePattern(Resulting_Matrix);
-    solverLU.factorize(Resulting_Matrix);
-    if (solverLU.info() == Eigen::NumericalIssue)
-        std::cout<< solverLU.info() << Eigen::NumericalIssue <<  std::endl;
-    Eigen::VectorXd xLU = solverLU.solve(b);
+// TRY CHOLESKY DECOMPOSITION
+    bool chol_success = true;
 
-    std::chrono::steady_clock::time_point end_LU = std::chrono::steady_clock::now();               //end LU
+    if(symmetric.compare("symmetric") == 0){
+        std::chrono::steady_clock::time_point begin_LDLT = std::chrono::steady_clock::now();             //start LDLT
 
-    filewrite << "Time spent using LU decomposition = " << std::chrono::duration_cast<std::chrono::seconds>(end_LU - begin_LU).count() << "[s]" << std::endl;
+        Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>>   solverLDLT;
+        solverLDLT.analyzePattern(Resulting_Matrix);
+        solverLDLT.factorize(Resulting_Matrix);
+        Eigen::VectorXd xLDLT = solverLDLT.solve(b);
+        
+        
+        if(solverLDLT.info() != Eigen::Success){
+            std::cout << "LDLT fail" << std::endl;
+            chol_success = false;
+        }
 
-    double relative_errorLU = (xLU - xe).norm() / xe.norm();
-    filewrite << "The relative error using LU decomposition is: " << relative_errorLU << std::endl;
+        if(chol_success == true){
+        std::chrono::steady_clock::time_point end_LDLT = std::chrono::steady_clock::now();               //end LU LDLT
 
-     //windows
-    filewrite << "PeakWorkingSetSize(windows): " << PrintMemoryInfo(GetCurrentProcessId()) << " MB"<< std::endl;
-	 //fine windows 
-	 
-    /*//linux
-    struct rusage rusage;
-    getrusage( RUSAGE_SELF, &rusage );
-    filewrite << "PeakWorkingSetSize(linux): " << (size_t)(rusage.ru_maxrss * 1024L)/1000000 << " MB"<< std::endl;
-	//fine linux*/
-	
-    filewrite.close();
+        filewrite << "Time spent using LDLT decomposition = " << std::chrono::duration_cast<std::chrono::seconds>(end_LDLT - begin_LDLT).count() << "[s]" << std::endl;
 
-    std::ofstream writeSolution;
-    std::string sol = " solution.txt";
-    writeSolution.open(__s + sol);
-    writeSolution << xLU - xe << std::endl;
-    writeSolution.close();
+        double relative_errorLDLT = (xLDLT - xe).norm() / xe.norm();
+        filewrite << "The relative error using LDLT decomposition is: " << relative_errorLDLT << std::endl;
+
+        //windows
+        filewrite << "PeakWorkingSetSize(windows): " << PrintMemoryInfo(GetCurrentProcessId()) << " MB"<< std::endl;
+        //fine windows 
+        
+        /*//linux
+        struct rusage rusage;
+        getrusage( RUSAGE_SELF, &rusage );
+        filewrite << "PeakWorkingSetSize(linux): " << (size_t)(rusage.ru_maxrss * 1024L)/1000000 << " MB"<< std::endl;
+        //fine linux*/
+        
+        filewrite.close();
+
+        std::ofstream writeSolution;
+        std::string sol = " solution.txt";
+        writeSolution.open(__s + sol);
+        writeSolution << xLDLT - xe << std::endl;
+        writeSolution.close();
+        }
+    }
+
+    if ((chol_success == false && symmetric.compare("symmetric") == 0) || symmetric.compare("non_symmetric") == 0) {
+
+        //LU DECOMPOSITION
+
+        std::chrono::steady_clock::time_point begin_LU = std::chrono::steady_clock::now();             //start LU
+
+        Eigen::SparseLU<Eigen::SparseMatrix<double>> solverLU; //, Eigen::COLAMDOrdering<int> 
+        solverLU.analyzePattern(Resulting_Matrix);
+        solverLU.factorize(Resulting_Matrix);
+        Eigen::VectorXd xLU = solverLU.solve(b);
+
+        std::chrono::steady_clock::time_point end_LU = std::chrono::steady_clock::now();               //end LU
+
+        filewrite << "Time spent using LU decomposition = " << std::chrono::duration_cast<std::chrono::seconds>(end_LU - begin_LU).count() << "[s]" << std::endl;
+
+        double relative_errorLU = (xLU - xe).norm() / xe.norm();
+        filewrite << "The relative error using LU decomposition is: " << relative_errorLU << std::endl;
+
+        //windows
+        filewrite << "PeakWorkingSetSize(windows): " << PrintMemoryInfo(GetCurrentProcessId()) << " MB"<< std::endl;
+        //fine windows 
+        
+        /*//linux
+        struct rusage rusage;
+        getrusage( RUSAGE_SELF, &rusage );
+        filewrite << "PeakWorkingSetSize(linux): " << (size_t)(rusage.ru_maxrss * 1024L)/1000000 << " MB"<< std::endl;
+        //fine linux*/
+        
+        filewrite.close();
+
+        std::ofstream writeSolution;
+        std::string sol = " solution.txt";
+        writeSolution.open(__s + sol);
+        writeSolution << xLU - xe << std::endl;
+        writeSolution.close();
+    }
 
 }
-
 
 int main(){
     
     //eseguire una matrice alla volta per stabilire il picco di ram esatto(sennò torna solo quello maggiore)
 
-    //solve_LU("GT01R.mtx", "non_symmetric");
-    //solve_LU("TSC_OPF_1047.mtx", "symmetric");
-    //solve_LU("ns3Da.mtx", "non_symmetric");
-    //solve_CHOLESKY("bundle_adj.mtx");
-    //solve_LU("ifiss_mat.mtx", "non_symmetric");
-    solve_CHOLESKY("G3_circuit.mtx");
-    //solve_CHOLESKY("nd24k.mtx");
-    //solve_CHOLESKY("Hook_1498.mtx");
+    solve("GT01R.mtx", "non_symmetric");
+    //solve("TSC_OPF_1047.mtx", "symmetric");
+    //solve("ns3Da.mtx", "non_symmetric");
+    //solve("bundle_adj.mtx", "symmetric");
+    //solve("ifiss_mat.mtx", "non_symmetric");
+    //solve("G3_circuit.mtx", "symmetric");
+    //solve("nd24k.mtx", "symmetric");
+    //solve("Hook_1498.mtx", "symmetric");
 }
